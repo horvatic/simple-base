035f6eef98dfef657055174a51d2b5456e4cefc0
horvatic <sbhorvatic@gmail.com>
2022-07-19 18:30:18 +0000

    add delete

--- src/command/delete.rs
+++ src/command/delete.rs
@@ -1,8 +1,28 @@
 use serde_json::{Value, Result};
 
-pub fn delete_command(_data: Value) -> Result<Value> {
-    return serde_json::from_str(r#"
-    {
-        "result": "run delete"
-    }"#)
+use std::fs;
+
+use crate::command::command_path;
+
+const ID_KEY: &str = "id";
+
+pub fn delete_command(data: Value) -> Result<Value> {
+    match data[ID_KEY].as_str() {
+        Some(id) => {
+            match fs::remove_file(command_path::DIR.to_string() + "/" + id) {
+                Ok(_) => return serde_json::from_str(r#"
+                    {
+                        "result": "run delete"
+                    }"#),
+                Err(_) => return serde_json::from_str(r#"
+                {
+                    "result": "error running delete"
+                }"#),
+            }
+        }
+        None => return serde_json::from_str(r#"
+        {
+            "result": "error running delete"
+        }"#),
+    }
 }
