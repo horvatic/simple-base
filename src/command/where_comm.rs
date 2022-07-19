ff72d7c776a441ec1009ff1424aa21e6bdcfdb4c
horvatic <sbhorvatic@gmail.com>
2022-07-19 19:04:56 +0000

    add where command

--- src/command/where_comm.rs
+++ src/command/where_comm.rs
@@ -1,8 +1,33 @@
 use serde_json::{Value, Result};
 
-pub fn where_command(_data: Value) -> Result<Value> {
-    return serde_json::from_str(r#"
-    {
-        "result": "run where"
-    }"#)
+use std::fs::File;
+use std::io::prelude::*;
+
+use crate::command::command_path;
+
+pub fn where_command(data: Value) -> Result<Value> {
+    match data[command_path::ID_KEY].as_str() {
+        Some(id) => {
+            match File::open(command_path::DIR.to_string() + "/" + id) {
+                Ok(mut file) => {
+                    let mut contents = String::new();
+                    match file.read_to_string(&mut contents) {
+                    Ok(_) => return serde_json::from_str(contents.as_str()),
+                    Err(_) => return serde_json::from_str(r#"
+                    {
+                        "result": "error running where, can not find entry"
+                    }"#)
+                    }
+                },
+                Err(_) => return serde_json::from_str(r#"
+                {
+                    "result": "error running where, can not find entry"
+                }"#),
+            }
+        }
+        None => return serde_json::from_str(r#"
+        {
+            "result": "error running where"
+        }"#),
+    }
 }
